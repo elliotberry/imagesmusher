7bef69c6f74ea3e3a4fb6203d4606be47a91d920
Elliot Berry <elliot@realonlinecasinogambling.ru>
2022-09-16 20:37:37 -0400

    big updatez

--- cli.js
+++ cli.js
@@ -2,17 +2,18 @@
 import process from 'node:process';
 import imagemin from './im.js';
 import ora from 'ora';
-import plur from 'plur';
 import {homedir} from 'os';
 import path from 'path';
 import logger from './logger.js';
 import pluginOverride from './plugins.js';
 import fs from 'node:fs';
 
-const defaultOutDir = path.join(homedir(), 'imagemin');
-if (!fs.existsSync(defaultOutDir)){
-  fs.mkdirSync(defaultOutDir);
-}
+let currentDir = process.cwd();
+
+const defaultOutDir = currentDir;
+//if (!fs.existsSync(defaultOutDir)){
+//  fs.mkdirSync(defaultOutDir);
+//}
 var spinner;
 
 const run = async (input, outDir = defaultOutDir) => {
@@ -20,11 +21,11 @@ const run = async (input, outDir = defaultOutDir) => {
     spinner = ora('Starting');
     spinner.start();
     const plugins = await pluginOverride();
-    let files;
-    files = await imagemin({input: input, destination: outDir, plugins: plugins});
+
+    let numberOfFilesOutput = await imagemin({input: input, destination: outDir, plugins: plugins});
     spinner.stop();
 
-    logger.info(`${files.length} ${plur('image', files.length)} minified and written to ${outDir}`);
+    logger.info(`the number of files minified was ${numberOfFilesOutput}. written to ${outDir}`);
   } catch (error) {
     spinner.stop();
     logger.error(error);
--- im.js
+++ im.js
@@ -1,4 +1,5 @@
 import { readFile, writeFile } from 'node:fs/promises'
+import {existsSync} from 'node:fs'
 import path from 'path'
 import { fileTypeFromBuffer } from 'file-type'
 import { globby } from 'globby'
@@ -12,36 +13,58 @@ const replaceExt = (path, newExtension) => {
 }
 
 const handleFile = async (input, output, plugins) => {
-    let filePath = path.resolve(input)
-    let data = await readFile(filePath)
-    const dest = path.join(output, path.basename(input))
+    let ret = false
+    try {
+        let filePath = path.resolve(input)
+        let data = await readFile(filePath)
+      
+        const {name, ext, dir} = path.parse(input);
+        const dest = path.join(output, `${name}-smashed${ext}`);
 
-    const pipe =
-        plugins.length > 0 ? promisePipe(plugins)(data) : Promise.resolve(data)
-    let buf = await pipe
-    buf = buf.length < data.length ? buf : data
+        const pipe =
+            plugins.length > 0
+                ? promisePipe(plugins)(data)
+                : Promise.resolve(data)
+        let buf = await pipe
+        buf = buf.length < data.length ? buf : data
 
-    let outputPath =
-        fileTypeFromBuffer(buf) && fileTypeFromBuffer(buf).ext === 'webp'
-            ? replaceExt(dest, '.webp')
-            : dest
-    await writeFile(outputPath, buf)
+        let outputPath =
+            fileTypeFromBuffer(buf) && fileTypeFromBuffer(buf).ext === 'webp'
+                ? replaceExt(dest, '.webp')
+                : dest
+        let outputFilename = checkFileExists(outputPath);
+        if (outputFilename !== outputPath) {
+            logger.info(`file ${outputPath} already exists. writing to ${outputFilename}`)
+        }
+        await writeFile(outputFilename, buf, { flag: 'wx' })
+        ret = true
+    } catch (e) {
+        logger.error(`issue with ${input}: ${e}`)
+    }
+    return ret
+}
+//function to check if destination filename exists, and if so, to rename it with a number
+const checkFileExists = (filename) => {
+    let i = 1
+    let newFilename = filename
+    while (existsSync(newFilename)) {
+        newFilename = filename.replace(/(\.[^\.]+)$/, `(${i})$1`)
+        i++
+    }
+    return newFilename
 }
 
 const imagemin = async ({ input, destination, plugins }) => {
-    try {
-        let paths = await globby(input, {
-            expandDirectories: {
-                extensions: ['png'],
-            },
-        })
-        let r = await Promise.all(
-            paths.map((filePath) => handleFile(filePath, destination, plugins))
-        )
-        return r
-    } catch (e) {
-        logger.error(e)
-    }
+    let paths = await globby(input, {
+        expandDirectories: {
+            extensions: ['png'],
+        },
+    })
+    let filesOutput = await Promise.all(
+        paths.map((filePath) => handleFile(filePath, destination, plugins))
+    )
+    let filesSucceeded = filesOutput.filter((x) => x === true).length
+    return filesSucceeded
 }
 
 const buffer = (input, opts) => {
