451945f4e4769300084df5e83e1ee102887c82da
Elliot Berry <elliot@daddy.cat>
2022-11-13 16:28:27 -0500

    added macosx automator workflow, as well as some additional fraudulent project badges.

--- cli.js
+++ cli.js
@@ -6,10 +6,40 @@ import chalk from 'chalk'
 import pluginOverride from './plugins.js'
 import yargs from 'yargs/yargs'
 import { existsSync, mkdirSync } from 'node:fs'
+import notifier from 'node-notifier'
+
+const agentIsMacosAutomator = () => {
+    let returnVal = false
+    if (process.env['XPC_SERVICE_NAME']) {
+        if (
+            process.env['XPC_SERVICE_NAME']
+                .toLowerCase()
+                .indexOf('com.apple.automator') > -1
+        ) {
+            returnVal = true
+        }
+    }
+    return returnVal
+}
+//const getOutputDir = (input) => {
 
 const defaultOutDir = process.cwd()
 
+const log = (message) => {
+    if (!agentIsMacosAutomator()) {
+        console.log(message)
+    }
+    else {
+        notifier.notify({
+            title: 'imagemin',
+            message: message,
+        })
+    }
+}
+
 const run = async () => {
+    let globPattern;
+    let outputDir = defaultOutDir;
     var argv = yargs(process.argv.slice(2))
         .usage('Usage: $0 <glob> [options]')
         .alias('o', 'output')
@@ -17,14 +47,19 @@ const run = async () => {
         .example([['$0 "./pictures-of-cats/*.png"', 'self-explanatory']])
         .version(process.env.npm_package_version).argv
 
-    const globPattern = argv._[0]
-    if (argv.output) {
-        if (!existsSync(argv.output)) {
-            mkdirSync(argv.output, { recursive: true })
-        }
-    }
+    globPattern = argv._[0]
 
-    const outputDir = argv.output || defaultOutDir
+    if (agentIsMacosAutomator()) {
+        globPattern = process.argv.slice(2);
+        outputDir = path.dirname(globPattern[0])
+    }
+    
+    if (argv.output) {
+        outputDir = argv.output
+    }
+    if (!existsSync(outputDir)) {
+        mkdirSync(outputDir, { recursive: true })
+    }
 
     try {
         const plugins = await pluginOverride()
@@ -34,11 +69,17 @@ const run = async () => {
             plugins: plugins,
         })
 
-        console.log(
-           `${numberOfFilesOutput > 0 ? chalk.green(numberOfFilesOutput) : chalk.red(numberOfFilesOutput)} ${chalk.green(`file(s) were minified and saved to ${path.resolve(outputDir)}`)}`)
-        
+        log(
+            `${
+                numberOfFilesOutput > 0
+                    ? chalk.green(numberOfFilesOutput)
+                    : chalk.red(numberOfFilesOutput)
+            } ${chalk.green(
+                `file(s) were minified and saved to ${path.resolve(outputDir)}`
+            )}`
+        )
     } catch (error) {
-        console.error(error)
+        log(`error: ${error}`)
     }
 }
 run()
--- readme.md
+++ readme.md
@@ -1,11 +1,19 @@
 
 # imagesmusher
 
-[![Total Downloads](https://poser.pugx.org/aimeos/aimeos-typo3/d/total.svg)](https://packagist.org/packages/aimeos/aimeos-typo3)
-[![](https://shields.io/badge/docker%20pulls-765M-066da5)](https://scrutinizer-ci.com/g/aimeos/aimeos-typo3/?branch=master)
-[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+[![Total Downloads](https://poser.pugx.org/aimeos/aimeos-typo3/d/total.svg)]()
+[![](https://shields.io/badge/docker%20pulls-765M-066da5)]()
+[![](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+[![](https://img.shields.io/badge/active%20installs-300k-brightgreen)]()
+[![](https://img.shields.io/badge/uptime-99.999%25-green)]()
+[![](https://img.shields.io/badge/favorites-20k-brightgreen)]()
+[![](https://img.shields.io/badge/endorsement-approved-green)]()
+[![](https://img.shields.io/badge/subscribers-15k-red?logo=youtube&style=social)]()
 
 ![ha](https://i.ibb.co/LdtzYpF/deepfried-1663466964672.jpg)
 
 my version of imagemin-cli. should "just work". You can't edit any plugins because I'm not very nice.
+
+## Macos Automator
+Example workflow included. client should detect arguments being passed from automator/macosx services by default, and then bubbles up results/correct output folder based on this type of interaction.
 
